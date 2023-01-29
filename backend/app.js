8ae6f822392a67c8e34921195fa716b82d1f228c
jivtesh singh <pahuls017@gmail.com>
2023-01-29 16:39:48 +0530

    deployement

--- backend/app.js
+++ backend/app.js
@@ -34,6 +34,7 @@ if(process.env.NODE_ENV === "production"){
   app.get('*',(req,res) => {
     res.sendFile(path.resolve(___dirname1,"frontend","build","index.html"));
   })
+
 }else{
   app.get("/", (req,res) => {
       res.send("API is running");
